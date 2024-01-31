37bf25b52bf2ebb9039bb987a446a6b51c096930
Timo Nolle <t.nolle@autodarts.io>
2024-01-31 21:45:47 +0100

    Add login function

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from time import sleep
 
 from keycloak import KeycloakOpenID
 
-CLIENT_ID = ""
-CLIENT_SECRET = ""
-
+CLIENT_ID = "timo-test"
+CLIENT_SECRET = "IX9MXMafb4OCCLrfpvyMqWSJFUXwLumI"
 
 class AutodartsKeycloakClient:
     token_lifetime_fraction = 0.9
@@ -15,6 +14,9 @@ class AutodartsKeycloakClient:
     refresh_token: str = None
     expires_at: datetime = None
     refresh_expires_at: datetime = None
+
+    username: str = None
+    password: str = None
 
     def __init__(self, *, client_id: str, client_secret: str = None):
         self.kc = KeycloakOpenID(
@@ -35,24 +37,29 @@ class AutodartsKeycloakClient:
             seconds=int(self.token_lifetime_fraction * token["refresh_expires_in"])
         )
 
-    def __get_token(self, username: str, password: str):
-        self.__set_token(self.kc.token(username, password))
+    def __get_token(self):
+        self.__set_token(self.kc.token(self.username, self.password))
         print("Getting token", self.expires_at, self.refresh_expires_at)
 
-    def __refresh_token(self    ):
+    def __refresh_token(self):
         self.__set_token(self.kc.refresh_token(self.refresh_token))
         print("Refreshing token", self.expires_at, self.refresh_expires_at)
 
-    def get_token(self, username: str, password: str):
+    def login(self, username: str, password: str):
+        self.username = username
+        self.password = password
+        self.__get_token()
+
+    def get_token(self):
         if self.access_token is None:
-            self.__get_token(username, password)
+            self.__get_token()
 
         now = datetime.now()
         if self.expires_at < now:
             if now < self.refresh_expires_at:
                 self.__refresh_token()
             else:
-                self.__get_token(username, password)
+                self.__get_token()
 
         return self.access_token
 
@@ -63,6 +70,8 @@ class AutodartsKeycloakClient:
 if __name__ == '__main__':
     kc = AutodartsKeycloakClient(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
+    kc.login("demo", "demo123")
+
     while True:
-        kc.get_token('demo', 'demo')
+        kc.get_token()
         sleep(1)
